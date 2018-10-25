9c772f9c92203bd7864fbba9d03ffa24041c52df
Sara Anspach <sara.anspach@gmail.com>
2018-10-25 10:58:12 -0300

    second

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DespicableMeCards from './components/DespicableMeCards';
+import NavBar from './components/NavBar';
+import Wrapper from './components/Wrapper';
+import Intro from './components/Intro';
+import despicable from './despicable.json';
+
+
+shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+
 
 class App extends Component {
   render() {

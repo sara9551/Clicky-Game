e974aa917b8627ca23b7c757cdd6343a4eb3884f
Sara Anspach <sara.anspach@gmail.com>
2018-10-26 09:02:55 -0300

    eigth

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -13,17 +13,16 @@ class App extends Component {
   };
 
   clickCard = id => {
-    // Filter this.state.despicable for despicable with an id not equal to the id being removed
+    // Filter this.state.despicable for despicable with an id not equal to the id
     const despicable = this.state.despicable.filter(despicables => despicables.id !== id);
-    // Set this.state.despicable equal to the new despicable array
-    this.setState({ despicable });
   };
 
-
+  //shuffle the animal cards
   shuffleAnimals = id => {
+  //use for loop for the shuffle of the cards
     for (let i = id.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [id[i], id[j]] = [id[j], id[i]];
+  //use this to have it return a random card
+    Math.floor(Math.random() * (i + 1));
   }
 }
 

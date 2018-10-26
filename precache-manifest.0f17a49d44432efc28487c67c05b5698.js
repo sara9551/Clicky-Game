91ed9a1dd928bfd01edad569986facbfcb1023f8
Sara Anspach <sara.anspach@gmail.com>
2018-10-26 12:04:35 -0300

    Updates

---
+++ precache-manifest.0f17a49d44432efc28487c67c05b5698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "e8daee98b13651a91e37",
+    "url": "/Clicky-Game/static/js/main.e8daee98.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/Clicky-Game/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "e8daee98b13651a91e37",
+    "url": "/Clicky-Game/static/css/main.4f95e5ea.chunk.css"
+  },
+  {
+    "revision": "ac556e5bd6185f805a5c41a6eedbaddb",
+    "url": "/Clicky-Game/index.html"
+  }
+];
